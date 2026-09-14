7dae560d12ba507929bed5c818fe338b0e81cce3
lovekushradhe28 <gurjarpapla498@gmail.com>
2026-09-14 06:19:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1004411643684"))
 # //FORCE_CHANNEL_ID
 CHANNEL_ID2 = int(os.environ.get("CHANNEL_ID2", "-1004411643684")) 
 # ------------------------------------------------
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://besib69802:YMOfgvnyjbRgW5qt@cluster0.yzzu2gn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://gurjarpapla498_db_user:11JKk8sD0Ng4GXpy@cluster0.dfpojps.mongodb.net/?appName=Cluster0")
 # -----------------------------------------------
 PREMIUM_LOGS = int(os.environ.get("PREMIUM_LOGS", "-1004411643684"))
 # -----------------------------------------------
